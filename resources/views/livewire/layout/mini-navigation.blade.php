6769871605aadd92cb5b2c16c634181f6f7325d8
Sloth <squeidish@gmail.com>
2025-03-21 12:34:43 +0630

    mii nav edited

--- resources/views/livewire/layout/mini-navigation.blade.php
+++ resources/views/livewire/layout/mini-navigation.blade.php
@@ -23,7 +23,7 @@ $logout = function (Logout $logout) {
                 </div>
 
                 {{-- Config links  --}}
-                @if (auth()->user()->hasAnyRole(['admin']))
+                @haspermission('company_access')
                     <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                         <div class="hidden sm:flex sm:items-center">
                             <x-dropdown align="right" width="48">
@@ -67,79 +67,79 @@ $logout = function (Logout $logout) {
                             </x-dropdown>
                         </div>
                     </div>
-                @endif
+                @endhaspermission
 
                 {{-- Config links  --}}
-                @if (auth()->user()->hasAnyRole(['admin']))
-                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                        <div class="hidden sm:flex sm:items-center">
-                            <x-dropdown align="right" width="48">
-                                <x-slot name="trigger">
-                                    <button
-                                        class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md dark:text-gray-400 dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none">
-                                        <div x-data="{ name: 'Configuration' }" x-text="name"
-                                            x-on:profile-updated.window="name = $event.detail.name"></div>
-                                        <div class="ms-1">
-                                            <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 20 20">
-                                                <path fill-rule="evenodd"
-                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                    clip-rule="evenodd" />
-                                            </svg>
-                                        </div>
-                                    </button>
-                                </x-slot>
 
-                                <x-slot name="content">
-                                    <!-- Accessory Group-->
-                                    <x-dropdown-link :href="route('accessory-config')" wire:navigate>
-                                        {{ 'Accessory Group' }}
-                                    </x-dropdown-link>
-                                    <!-- Accessory -->
-                                    <x-dropdown-link :href="route('accessory')" wire:navigate>
-                                        {{ 'Accessory' }}
-                                    </x-dropdown-link>
-                                </x-slot>
-                            </x-dropdown>
-                        </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <div class="hidden sm:flex sm:items-center">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md dark:text-gray-400 dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none">
+                                    <div x-data="{ name: 'Configuration' }" x-text="name"
+                                        x-on:profile-updated.window="name = $event.detail.name"></div>
+                                    <div class="ms-1">
+                                        <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <!-- Accessory Group-->
+                                <x-dropdown-link :href="route('accessory-config')" wire:navigate>
+                                    {{ 'Accessory Group' }}
+                                </x-dropdown-link>
+                                <!-- Accessory -->
+                                <x-dropdown-link :href="route('accessory')" wire:navigate>
+                                    {{ 'Accessory' }}
+                                </x-dropdown-link>
+                            </x-slot>
+                        </x-dropdown>
                     </div>
-                @endif
+                </div>
+
 
                 {{-- Config links  --}}
-                @if (auth()->user()->hasAnyRole(['admin']))
-                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                        <div class="hidden sm:flex sm:items-center">
-                            <x-dropdown align="right" width="48">
-                                <x-slot name="trigger">
-                                    <button
-                                        class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md dark:text-gray-400 dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none">
-                                        <div x-data="{ name: 'Assets' }" x-text="name"
-                                            x-on:profile-updated.window="name = $event.detail.name"></div>
-                                        <div class="ms-1">
-                                            <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 20 20">
-                                                <path fill-rule="evenodd"
-                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                    clip-rule="evenodd" />
-                                            </svg>
-                                        </div>
-                                    </button>
-                                </x-slot>
+                @
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <div class="hidden sm:flex sm:items-center">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md dark:text-gray-400 dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none">
+                                    <div x-data="{ name: 'Assets' }" x-text="name"
+                                        x-on:profile-updated.window="name = $event.detail.name"></div>
+                                    <div class="ms-1">
+                                        <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
 
-                                <x-slot name="content">
-                                    <!-- Accessory Group-->
-                                    <x-dropdown-link :href="route('assets')" wire:navigate>
-                                        {{ 'Fix Assets' }}
-                                    </x-dropdown-link>
-                                    <!-- Accessory -->
-                                    {{-- <x-dropdown-link :href="route('accessory')" wire:navigate>
+                            <x-slot name="content">
+                                <!-- Accessory Group-->
+                                <x-dropdown-link :href="route('assets')" wire:navigate>
+                                    {{ 'Fix Assets' }}
+                                </x-dropdown-link>
+                                <!-- Accessory -->
+                                {{-- <x-dropdown-link :href="route('accessory')" wire:navigate>
                                       {{ 'Accessory' }}
                                   </x-dropdown-link> --}}
-                                </x-slot>
-                            </x-dropdown>
-                        </div>
+                            </x-slot>
+                        </x-dropdown>
                     </div>
-                @endif
+                </div>
+
 
                 {{-- Setting links  --}}
                 @if (auth()->user()->hasAnyRole(['admin']))
